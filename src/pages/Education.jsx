e2d09c0b1b25ba752150bcb8f5286c0f6f1c6ca7
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-04-01 10:00:05 +0500

    Education section

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Education() {
   return (
-    <div className='container mx-auto'>Education</div>
-  )
+    <div className="container mx-auto">
+      {/* ------------------- */}
+      <div className="flex flex-col justify-center items-center h-[500px]">
+        <h1 className="text-6xl text-center text-[#333]">
+          STUDY <b>ABROAD</b> IS EASY WITH US
+        </h1>
+        <ul className="flex justify-center items-center gap-5 my-5">
+          <li className="btn-primary-edu">UI design</li>
+          <li className="btn-primary-edu">Frontend</li>
+          <li className="btn-primary-edu">Backend</li>
+          <li className="btn-primary-edu">Mobile developer</li>
+          <li className="btn-primary-edu">Graphic design</li>
+          <li className="btn-primary-edu">UX design</li>
+        </ul>
+          <p className="text-center">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+          <button className="btn rounded-full uppercase mt-5 bg-gradient-to-r from-[#eed9ed]/50 to-[#2ec05a]/20 border-[#333] text-[#333]">get started</button>
+      </div>
+      {/* ------------------- */}
+    </div>
+  );
 }
 
-export default Education
+export default Education;

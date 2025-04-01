3d1e7dfb558d7e6ca9862598b826f85303ef251f
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-04-01 14:22:29 +0500

    Select course section - start

--- src/pages/SelectCourse.jsx
+++ src/pages/SelectCourse.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function SelectCourse() {
   return (
-    <div className='container mx-auto'>SelectCourse</div>
+    <div className='container mx-auto flex mt-4 gap-4'>
+
+      <div className='w-92 bg-white rounded-lg border'>
+
+      </div>
+
+      <div className='flex-1 rounded-lg border border-gray-300'></div>
+
+    </div>
   )
 }
 

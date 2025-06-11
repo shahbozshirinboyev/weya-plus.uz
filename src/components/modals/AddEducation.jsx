cabf643439eae28dc3f608854981eafdb023c687
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-06-11 09:58:46 +0500

    new section ...

--- src/components/modals/AddEducation.jsx
+++ src/components/modals/AddEducation.jsx
@@ -46,11 +46,13 @@ function AddEducation() {
           </legend>
 
           <div className="w-full rounded-md   bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10 ">
-            <select className="select w-full bg-transparent rounded-md  border border-gray-300 dark:border-gray-600">
-            <option disabled selected className="bg-gray-900"> Pick a color </option>
-                  <option className="bg-gray-900 text-gray-50">Crimson</option>
-                  <option className="bg-gray-900 text-gray-50">Amber</option>
-                  <option className="bg-gray-900 text-gray-50">Velvet</option>
+            <select defaultValue="" className="select w-full ...">
+              <option value="" disabled>
+                Taʼlim turini tanlang
+              </option>
+              <option value="crimson">Crimson</option>
+              <option value="amber">Amber</option>
+              <option value="velvet">Velvet</option>
             </select>
           </div>
 
@@ -89,13 +91,24 @@ function AddEducation() {
           </fieldset>
 
           <div className="modal-action w-full flex justify-end">
-            <button className="btn w-24 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600" > Save </button>
-            <button onClick={()=> { document.getElementById("add_education").close() }} className="btn w-24 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600" > Close </button>
+            <button className="btn w-24 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600">
+              {" "}
+              Save{" "}
+            </button>
+            <button
+              onClick={() => {
+                document.getElementById("add_education").close();
+              }}
+              className="btn w-24 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600"
+            >
+              {" "}
+              Close{" "}
+            </button>
           </div>
         </div>
         <form method="dialog" className="modal-backdrop">
-    <button>close</button>
-  </form>
+          <button>close</button>
+        </form>
       </dialog>
     </>
   );
--- src/components/modals/MyProfileInfoUpdate.jsx
+++ src/components/modals/MyProfileInfoUpdate.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function MyProfileInfoUpdate() {
   return (
     <>
@@ -14,8 +12,12 @@ function MyProfileInfoUpdate() {
 
             <div className="">
               <div className="flex gap-2">
-              <button className="btn btn-sm w-20 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600" > Upload </button>
-              <button className="btn btn-sm w-20 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600" > Delete </button>
+                <button className="btn btn-sm w-20 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600">
+                  Upload
+                </button>
+                <button className="btn btn-sm w-20 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600">
+                  Delete
+                </button>
               </div>
               <p className="text-xs opacity-50 mt-2">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
@@ -56,8 +58,13 @@ function MyProfileInfoUpdate() {
               <legend className="fieldset-legend">Пол</legend>
 
               <div className="w-full rounded-md   bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10 ">
-                <select className="select w-full bg-transparent rounded-md  border border-gray-300 dark:border-gray-600">
-                  <option disabled selected className="bg-gray-900"> Pick a color </option>
+                <select
+                  defaultValue=""
+                  className="select w-full bg-transparent rounded-md border border-gray-300 dark:border-gray-600"
+                >
+                  <option disabled value="" className="bg-gray-900">
+                    Pick a color
+                  </option>
                   <option className="bg-gray-900 text-gray-50">Crimson</option>
                   <option className="bg-gray-900 text-gray-50">Amber</option>
                   <option className="bg-gray-900 text-gray-50">Velvet</option>
@@ -66,12 +73,17 @@ function MyProfileInfoUpdate() {
             </fieldset>
           </div>
 
-          <div className=" ">
+          <div>
             <fieldset className="fieldset ">
               <legend className="fieldset-legend">Страна</legend>
               <div className="w-full rounded-md   bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10 ">
-                <select className="select w-full bg-transparent rounded-md  border border-gray-300 dark:border-gray-600">
-                  <option disabled selected className="bg-gray-900"> Pick a color </option>
+                <select
+                  defaultValue=""
+                  className="select w-full bg-transparent rounded-md border border-gray-300 dark:border-gray-600"
+                >
+                  <option disabled value="" className="bg-gray-900">
+                    Pick a color
+                  </option>
                   <option className="bg-gray-900 text-gray-50">Crimson</option>
                   <option className="bg-gray-900 text-gray-50">Amber</option>
                   <option className="bg-gray-900 text-gray-50">Velvet</option>
@@ -87,8 +99,17 @@ function MyProfileInfoUpdate() {
             </fieldset>
           </div>
           <div className="modal-action w-full flex justify-end">
-          <button className="btn w-24 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600" > Save </button>
-          <button onClick={()=> { document.getElementById("my_profile_info_update").close() }} className="btn w-24 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600" > Close </button>
+            <button className="btn w-24 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600">
+              Save
+            </button>
+            <button
+              onClick={() => {
+                document.getElementById("my_profile_info_update").close();
+              }}
+              className="btn w-24 rounded-full bg-gradient-to-r from-[#eed9ed]/50 dark:from-[#eed9ed]/10 to-[#2ec05a]/20 border border-gray-300 dark:border-gray-700 hover:border-gray-400 dark:hover:border-gray-600"
+            >
+              Close
+            </button>
           </div>
         </div>
         <form method="dialog" className="modal-backdrop">
